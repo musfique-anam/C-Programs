c761e4a76f9b443eaef3ae155b50509f347d8700
musfique-anam <musfique2002.personal@gmail.com>
2024-12-18 19:11:03 +0600

    Length of a String

---
+++ StringLength.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    printf("Length of the string: %lu\n", strlen(str));
+    return 0;
+}
